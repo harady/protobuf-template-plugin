62468e8a610af0224254267cfb900beb32e7176b
harady <harady15@gmail.com>
2022-09-19 20:12:18 +0900

    ヘルパーメソッド追加

--- protoc-gen-template/protoc-gen-myplugincsharp/Program.cs
+++ protoc-gen-template/protoc-gen-myplugincsharp/Program.cs
@@ -9,17 +9,10 @@ using Scriban;
 
 namespace protoc_gen_myplugincsharp
 {
-	// assume current directory is the output directory, and it contains protoc.exe.
-	// protoc.exe --plugin=protoc-gen-myplugincsharp.exe --myplugincsharp_out=./ --proto_path=%userprofile%\.nuget\packages\google.protobuf.tools\3.21.1\tools --proto_path=./ chat.proto
-
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			// you can attach debugger
-			// System.Diagnostics.Debugger.Launch();
-
-			// get request from standard input
 			CodeGeneratorRequest request;
 			using (var stdin = Console.OpenStandardInput()) {
 				request = Deserialize<CodeGeneratorRequest>(stdin);
@@ -38,30 +31,6 @@ namespace protoc_gen_myplugincsharp
 				.Where(file => fileToGenerates.Contains(file.Name));
 
 			foreach (var fileDesc in outputFileDescs) {
-				//var output = new StringBuilder();
-
-				//// make service method list
-				//foreach (var serviceType in fileDesc.Service) {
-				//	output.AppendLine($"service {serviceType.Name}");
-
-				//	foreach (var method in serviceType.Method) {
-				//		output.AppendLine($"   {method.OutputType} {method.Name}({method.InputType})");
-				//	}
-				//}
-
-				//// make message field list
-				//foreach (var messageDesc in fileDesc.MessageType) {
-				//	output.AppendLine($"message {messageDesc.Name}");
-
-				//	foreach (var fieldDesc in messageDesc.Field) {
-				//		output.AppendLine($"   {fieldDesc.TypeName} {fieldDesc.Name}");
-				//	}
-
-				//	foreach (var enumDesc in messageDesc.EnumType) {
-				//		output.AppendLine($"   {enumDesc.Name}");
-				//	}
-				//}
-
 				var filename
 					= Path.GetFileNameWithoutExtension(fileDesc.Name)
 						.ToPascalCase();
@@ -69,7 +38,9 @@ namespace protoc_gen_myplugincsharp
 
 				//fileDesc.MessageType[0].Field[0].TypeName;
 
-				var output = template.Render(new { File = fileDesc }); // => "Hello World!" 
+				var helper = new ScribanHelper();
+				var context = new TemplateContext(helper);
+				var output = template.Render(new { File = fileDesc });
 
 				// set as response
 				response.File.Add(
