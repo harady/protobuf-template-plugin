9174dfe13b309ab90a7df6e34c17f441eea786cb
harady <harady15@gmail.com>
2022-09-21 02:33:42 +0900

    修正

--- proto/codegen2_all.sh
+++ proto/codegen2_all.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+
+# サーバー側
+#ruby codegen.rb -i codegen2_server_cs.yml
+
 # Unity側
-ruby codegen.rb -i codegen_unity_cs2.yml
+ruby codegen.rb -i codegen2_unity_cs.yml
+
+# マスターデータ側
+#sh protoc_sheet.sh
 
 read -p "Press [Enter] key to resume."
