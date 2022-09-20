056ac82c33d8a162a9a070a9dca80ed07fea8845
harady <harady15@gmail.com>
2022-09-21 05:33:02 +0900

    Revert "処理時間を表示"
    This reverts commit 3d124a3ff95c937be040bb23a41031949c862ef2.

--- proto/codegen2_all.sh
+++ proto/codegen2_all.sh
@@ -8,9 +8,6 @@
 # codegen2_all.sh cp
 #================================
 
-# 現在時刻を取得
-_started_at=$(date +'%s.%3N')
-
 # 引数セットアップ.
 if [ $# = 0 ]; then
     param=""
@@ -33,16 +30,7 @@ ruby codegen.rb -i codegen2_unity_cs.yml
 #sh protoc_sheet.sh
 
 
-# 完了時刻を取得
-_ended_at=$(date +'%s.%3N')
-# 経過時間を計算
-_elapsed=$(echo "scale=3; $_ended_at - $_started_at" | bc)
-
 # 一時停止
 if [[ "$param" == *p* ]]; then
-  echo "start: $(date -d "@${_started_at}" +'%Y-%m-%d %H:%M:%S.%3N (%:z)')"
-  echo "end  : $(date -d "@${_ended_at}" +'%Y-%m-%d %H:%M:%S.%3N (%:z)')"
-  echo "dur:   $_elapsed"
-  eval "echo Elapsed Time: $(date -ud "@$_elapsed" +'$((%s/3600/24)):%H:%M:%S.%3N')"
   read -p "Press [Enter] key to resume."
 fi
