583fa4089599a890c5e64d35df3d4f8f6fdfa888
harady <harady15@gmail.com>
2022-09-24 08:22:53 +0900

    正しくBOMチェックできるようになった

--- protoc-gen-template/protoc-gen-myplugincsharp/BomChecker.cs
+++ protoc-gen-template/protoc-gen-myplugincsharp/BomChecker.cs
@@ -9,7 +9,7 @@
 		return IsBOM(bom, out var codepage);
 	}
 
-	static bool IsBOM(byte[] bomByte, out int codepage)
+	private static bool IsBOM(byte[] bomByte, out int codepage)
 	{
 		bool result;
 		byte[] bomUTF8 = { 0xEF, 0xBB, 0xBF };
@@ -18,25 +18,40 @@
 		byte[] bomUTF32Little = { 0xFF, 0xFE, 0x00, 0x00 };
 		byte[] bomUTF32Big = { 0x00, 0x00, 0xFE, 0xFF };
 
-		if (bomByte.SequenceEqual(bomUTF8)) {
+		if (IsMatched(bomByte, bomUTF8)) {
 			result = true;
 			codepage = 65001; //utf-8,Unicode (UTF-8)
-		} else if (bomByte.SequenceEqual(bomUTF32Little)) {
+		} else if (IsMatched(bomByte, bomUTF32Little)) {
 			result = true;
 			codepage = 12000; //utf-32,Unicode (UTF-32)
-		} else if (bomByte.SequenceEqual(bomUTF32Big)) {
+		} else if (IsMatched(bomByte, bomUTF32Big)) {
 			result = true;
 			codepage = 12001; //utf-32BE,Unicode (UTF-32 Big-Endian) 
-		} else if (bomByte.SequenceEqual(bomUTF16Little)) {
+		} else if (IsMatched(bomByte, bomUTF16Little)) {
 			result = true;
 			codepage = 1200; //utf-16,Unicode
-		} else if (bomByte.SequenceEqual(bomUTF16Big)) {
+		} else if (IsMatched(bomByte, bomUTF16Big)) {
 			result = true;
 			codepage = 1201; //utf-16BE,Unicode (Big-Endian) 
 		} else {
 			result = false;
 			//codepage = 0; //non BOM !
 			codepage = 932; //shift_jis,Japanese (Shift-JIS)
+		}
+
+		return result;
+	}
+
+
+	private static bool IsMatched(byte[] data, byte[] bom)
+	{
+		bool result = true;
+
+		for (int i = 0; i < bom.Length; i++) {
+			if (bom[i] != data[i]) {
+				result = false;
+				break;
+			}
 		}
 
 		return result;
