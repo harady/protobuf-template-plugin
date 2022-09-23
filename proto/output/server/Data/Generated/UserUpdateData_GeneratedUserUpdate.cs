a8a6c0951a0e8a829085673af237213f931b1f32
harady <harady15@gmail.com>
2022-09-23 22:02:16 +0900

    case修正

--- proto/output/server/Data/Generated/UserUpdateData_GeneratedUserUpdate.cs
+++ proto/output/server/Data/Generated/UserUpdateData_GeneratedUserUpdate.cs
@@ -32,113 +32,113 @@ namespace AwsDotnetCsharp
 				this.user = toMerge.user;
 			}
 			this.userBackups.Merge(toMerge.userBackups);
-			this.deleteduser_backupsIds.Merge(toMerge.deleteduser_backupsIds);
+			this.deletedUserBackupIds.Merge(toMerge.deletedUserBackupIds);
 			this.userDecks.Merge(toMerge.userDecks);
-			this.deleteduser_decksIds.Merge(toMerge.deleteduser_decksIds);
+			this.deletedUserDeckIds.Merge(toMerge.deletedUserDeckIds);
 			this.userExchangeItems.Merge(toMerge.userExchangeItems);
-			this.deleteduser_exchange_itemsIds.Merge(toMerge.deleteduser_exchange_itemsIds);
+			this.deletedUserExchangeItemIds.Merge(toMerge.deletedUserExchangeItemIds);
 			this.userGachaButtons.Merge(toMerge.userGachaButtons);
-			this.deleteduser_gacha_buttonsIds.Merge(toMerge.deleteduser_gacha_buttonsIds);
+			this.deletedUserGachaButtonIds.Merge(toMerge.deletedUserGachaButtonIds);
 			this.userItems.Merge(toMerge.userItems);
-			this.deleteduser_itemsIds.Merge(toMerge.deleteduser_itemsIds);
+			this.deletedUserItemIds.Merge(toMerge.deletedUserItemIds);
 			this.userMessages.Merge(toMerge.userMessages);
-			this.deleteduser_messagesIds.Merge(toMerge.deleteduser_messagesIds);
+			this.deletedUserMessageIds.Merge(toMerge.deletedUserMessageIds);
 			this.userMissions.Merge(toMerge.userMissions);
-			this.deleteduser_missionsIds.Merge(toMerge.deleteduser_missionsIds);
+			this.deletedUserMissionIds.Merge(toMerge.deletedUserMissionIds);
 			this.userPaidCrystals.Merge(toMerge.userPaidCrystals);
-			this.deleteduser_paid_crystalsIds.Merge(toMerge.deleteduser_paid_crystalsIds);
+			this.deletedUserPaidCrystalIds.Merge(toMerge.deletedUserPaidCrystalIds);
 			this.userShopItems.Merge(toMerge.userShopItems);
-			this.deleteduser_shop_itemsIds.Merge(toMerge.deleteduser_shop_itemsIds);
+			this.deletedUserShopItemIds.Merge(toMerge.deletedUserShopItemIds);
 			this.userStages.Merge(toMerge.userStages);
-			this.deleteduser_stagesIds.Merge(toMerge.deleteduser_stagesIds);
+			this.deletedUserStageIds.Merge(toMerge.deletedUserStageIds);
 			this.userUnits.Merge(toMerge.userUnits);
-			this.deleteduser_unitsIds.Merge(toMerge.deleteduser_unitsIds);
+			this.deletedUserUnitIds.Merge(toMerge.deletedUserUnitIds);
 			this.userUnitCollections.Merge(toMerge.userUnitCollections);
-			this.deleteduser_unit_collectionsIds.Merge(toMerge.deleteduser_unit_collectionsIds);
+			this.deletedUserUnitCollectionIds.Merge(toMerge.deletedUserUnitCollectionIds);
 		}
 
-		[BsonElement("deleteduser_backupsIds")]
-		[DataMember(Name = "deleteduser_backupsIds")]
-		public List<long> deleteduser_backupsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserBackupIds")]
+		[DataMember(Name = "deletedUserBackupIds")]
+		public List<long> deletedUserBackupIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_decksIds")]
-		[DataMember(Name = "deleteduser_decksIds")]
-		public List<long> deleteduser_decksIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserDeckIds")]
+		[DataMember(Name = "deletedUserDeckIds")]
+		public List<long> deletedUserDeckIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_exchange_itemsIds")]
-		[DataMember(Name = "deleteduser_exchange_itemsIds")]
-		public List<long> deleteduser_exchange_itemsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserExchangeItemIds")]
+		[DataMember(Name = "deletedUserExchangeItemIds")]
+		public List<long> deletedUserExchangeItemIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_gacha_buttonsIds")]
-		[DataMember(Name = "deleteduser_gacha_buttonsIds")]
-		public List<long> deleteduser_gacha_buttonsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserGachaButtonIds")]
+		[DataMember(Name = "deletedUserGachaButtonIds")]
+		public List<long> deletedUserGachaButtonIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_itemsIds")]
-		[DataMember(Name = "deleteduser_itemsIds")]
-		public List<long> deleteduser_itemsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserItemIds")]
+		[DataMember(Name = "deletedUserItemIds")]
+		public List<long> deletedUserItemIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_messagesIds")]
-		[DataMember(Name = "deleteduser_messagesIds")]
-		public List<long> deleteduser_messagesIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserMessageIds")]
+		[DataMember(Name = "deletedUserMessageIds")]
+		public List<long> deletedUserMessageIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_missionsIds")]
-		[DataMember(Name = "deleteduser_missionsIds")]
-		public List<long> deleteduser_missionsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserMissionIds")]
+		[DataMember(Name = "deletedUserMissionIds")]
+		public List<long> deletedUserMissionIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_paid_crystalsIds")]
-		[DataMember(Name = "deleteduser_paid_crystalsIds")]
-		public List<long> deleteduser_paid_crystalsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserPaidCrystalIds")]
+		[DataMember(Name = "deletedUserPaidCrystalIds")]
+		public List<long> deletedUserPaidCrystalIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_shop_itemsIds")]
-		[DataMember(Name = "deleteduser_shop_itemsIds")]
-		public List<long> deleteduser_shop_itemsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserShopItemIds")]
+		[DataMember(Name = "deletedUserShopItemIds")]
+		public List<long> deletedUserShopItemIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_stagesIds")]
-		[DataMember(Name = "deleteduser_stagesIds")]
-		public List<long> deleteduser_stagesIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserStageIds")]
+		[DataMember(Name = "deletedUserStageIds")]
+		public List<long> deletedUserStageIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_unitsIds")]
-		[DataMember(Name = "deleteduser_unitsIds")]
-		public List<long> deleteduser_unitsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserUnitIds")]
+		[DataMember(Name = "deletedUserUnitIds")]
+		public List<long> deletedUserUnitIds { get; set; } = new List<long>();
 
-		[BsonElement("deleteduser_unit_collectionsIds")]
-		[DataMember(Name = "deleteduser_unit_collectionsIds")]
-		public List<long> deleteduser_unit_collectionsIds { get; set; } = new List<long>();
+		[BsonElement("deletedUserUnitCollectionIds")]
+		[DataMember(Name = "deletedUserUnitCollectionIds")]
+		public List<long> deletedUserUnitCollectionIds { get; set; } = new List<long>();
 
-		public TableUpdateData<UserBackupData> user_backupsTableUpdate
-			=> new TableUpdateData<UserBackupData>(user_backupss, deleteduser_backupsIds);
+		public TableUpdateData<UserBackupData> userBackupTableUpdate
+			=> new TableUpdateData<UserBackupData>(userBackups, deletedUserBackupIds);
 
-		public TableUpdateData<UserDeckData> user_decksTableUpdate
-			=> new TableUpdateData<UserDeckData>(user_deckss, deleteduser_decksIds);
+		public TableUpdateData<UserDeckData> userDeckTableUpdate
+			=> new TableUpdateData<UserDeckData>(userDecks, deletedUserDeckIds);
 
-		public TableUpdateData<UserExchangeItemData> user_exchange_itemsTableUpdate
-			=> new TableUpdateData<UserExchangeItemData>(user_exchange_itemss, deleteduser_exchange_itemsIds);
+		public TableUpdateData<UserExchangeItemData> userExchangeItemTableUpdate
+			=> new TableUpdateData<UserExchangeItemData>(userExchangeItems, deletedUserExchangeItemIds);
 
-		public TableUpdateData<UserGachaButtonData> user_gacha_buttonsTableUpdate
-			=> new TableUpdateData<UserGachaButtonData>(user_gacha_buttonss, deleteduser_gacha_buttonsIds);
+		public TableUpdateData<UserGachaButtonData> userGachaButtonTableUpdate
+			=> new TableUpdateData<UserGachaButtonData>(userGachaButtons, deletedUserGachaButtonIds);
 
-		public TableUpdateData<UserItemData> user_itemsTableUpdate
-			=> new TableUpdateData<UserItemData>(user_itemss, deleteduser_itemsIds);
+		public TableUpdateData<UserItemData> userItemTableUpdate
+			=> new TableUpdateData<UserItemData>(userItems, deletedUserItemIds);
 
-		public TableUpdateData<UserMessageData> user_messagesTableUpdate
-			=> new TableUpdateData<UserMessageData>(user_messagess, deleteduser_messagesIds);
+		public TableUpdateData<UserMessageData> userMessageTableUpdate
+			=> new TableUpdateData<UserMessageData>(userMessages, deletedUserMessageIds);
 
-		public TableUpdateData<UserMissionData> user_missionsTableUpdate
-			=> new TableUpdateData<UserMissionData>(user_missionss, deleteduser_missionsIds);
+		public TableUpdateData<UserMissionData> userMissionTableUpdate
+			=> new TableUpdateData<UserMissionData>(userMissions, deletedUserMissionIds);
 
-		public TableUpdateData<UserPaidCrystalData> user_paid_crystalsTableUpdate
-			=> new TableUpdateData<UserPaidCrystalData>(user_paid_crystalss, deleteduser_paid_crystalsIds);
+		public TableUpdateData<UserPaidCrystalData> userPaidCrystalTableUpdate
+			=> new TableUpdateData<UserPaidCrystalData>(userPaidCrystals, deletedUserPaidCrystalIds);
 
-		public TableUpdateData<UserShopItemData> user_shop_itemsTableUpdate
-			=> new TableUpdateData<UserShopItemData>(user_shop_itemss, deleteduser_shop_itemsIds);
+		public TableUpdateData<UserShopItemData> userShopItemTableUpdate
+			=> new TableUpdateData<UserShopItemData>(userShopItems, deletedUserShopItemIds);
 
-		public TableUpdateData<UserStageData> user_stagesTableUpdate
-			=> new TableUpdateData<UserStageData>(user_stagess, deleteduser_stagesIds);
+		public TableUpdateData<UserStageData> userStageTableUpdate
+			=> new TableUpdateData<UserStageData>(userStages, deletedUserStageIds);
 
-		public TableUpdateData<UserUnitData> user_unitsTableUpdate
-			=> new TableUpdateData<UserUnitData>(user_unitss, deleteduser_unitsIds);
+		public TableUpdateData<UserUnitData> userUnitTableUpdate
+			=> new TableUpdateData<UserUnitData>(userUnits, deletedUserUnitIds);
 
-		public TableUpdateData<UserUnitCollectionData> user_unit_collectionsTableUpdate
-			=> new TableUpdateData<UserUnitCollectionData>(user_unit_collectionss, deleteduser_unit_collectionsIds);
+		public TableUpdateData<UserUnitCollectionData> userUnitCollectionTableUpdate
+			=> new TableUpdateData<UserUnitCollectionData>(userUnitCollections, deletedUserUnitCollectionIds);
 	}
 }
