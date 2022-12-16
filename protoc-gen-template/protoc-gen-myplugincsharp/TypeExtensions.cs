21e40300fd86e3de212ed4e78f495c0eb2a3b3dd
harady <harady15@gmail.com>
2022-12-17 07:40:43 +0900

    Ruby型変換実装中

--- protoc-gen-template/protoc-gen-myplugincsharp/TypeExtensions.cs
+++ protoc-gen-template/protoc-gen-myplugincsharp/TypeExtensions.cs
@@ -8,10 +8,10 @@ public static class TypeExtensions
 	public static string ToCsTypeName(
 		this FieldDescriptorProto.Types.Type type)
 	{
-		return typeDict.GetValueOrDefault(type, "");
+		return typeCsTypeDict.GetValueOrDefault(type, "");
 	}
 
-	static Dictionary<FieldDescriptorProto.Types.Type, string> typeDict
+	static Dictionary<FieldDescriptorProto.Types.Type, string> typeCsTypeDict
 		= new Dictionary<FieldDescriptorProto.Types.Type, string> {
 			{ FieldDescriptorProto.Types.Type.Double, "double" },
 			{ FieldDescriptorProto.Types.Type.Float, "float" },
@@ -31,5 +31,39 @@ public static class TypeExtensions
 			{ FieldDescriptorProto.Types.Type.Sint32, "int" },
 			{ FieldDescriptorProto.Types.Type.Sint64, "long" },
 		};
+
+	public static string ToRbTypeName(
+		this FieldDescriptorProto.Types.Type type)
+	{
+		return typeRbTypeDict.GetValueOrDefault(type, "");
+	}
+
+	static Dictionary<FieldDescriptorProto.Types.Type, string> typeRbTypeDict
+		= new Dictionary<FieldDescriptorProto.Types.Type, string> {
+			{ FieldDescriptorProto.Types.Type.Double, "double" },
+			{ FieldDescriptorProto.Types.Type.Float, "float" },
+			{ FieldDescriptorProto.Types.Type.Int64, "long" },
+			{ FieldDescriptorProto.Types.Type.Fixed64, "ulong" },
+			{ FieldDescriptorProto.Types.Type.Int32, "int" },
+			{ FieldDescriptorProto.Types.Type.Fixed32, "uint" },
+			{ FieldDescriptorProto.Types.Type.Bool, "bool" },
+			{ FieldDescriptorProto.Types.Type.String, "string" },
+			{ FieldDescriptorProto.Types.Type.Group, "GROUP" },
+			{ FieldDescriptorProto.Types.Type.Message, "MESSAGE" },
+			{ FieldDescriptorProto.Types.Type.Bytes, "Google.Protobuf.ByteString" },
+			{ FieldDescriptorProto.Types.Type.Uint32, "uint" },
+			{ FieldDescriptorProto.Types.Type.Enum, "ENUM" },
+			{ FieldDescriptorProto.Types.Type.Sfixed32, "int" },
+			{ FieldDescriptorProto.Types.Type.Sfixed64, "long" },
+			{ FieldDescriptorProto.Types.Type.Sint32, "int" },
+			{ FieldDescriptorProto.Types.Type.Sint64, "long" },
+		};
+
+	public static string ToRbTypeDefaultValue(
+		this FieldDescriptorProto.Types.Type type)
+	{
+		return typeCsTypeDict.GetValueOrDefault(type, "");
+	}
+
 
 }
